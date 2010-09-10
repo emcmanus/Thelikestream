a0d919b8a90ccb9d88f7e6eb63cb315b839a7579
Ed McManus <edmcmanus@champ.local>
2010-09-09 22:58:54 -0700

    gem config fix

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "nokogiri"
   config.gem "stringex"
   config.gem "mapel"
-  config.gem "aws-s3"
+  config.gem "aws/s3"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
