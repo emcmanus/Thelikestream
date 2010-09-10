96cedf8928d14a4cbf0f74c876804318e52969ba
Ed McManus <edmcmanus@champ.local>
2010-09-09 21:04:37 -0700

    Cap updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,26 @@
-set :application, "likestream"
-set :repository,  "ssh://git@184.106.218.75:2298/home/git/likestream"
-set :port, 2298
+set :application, 	"likestream"
+set :domain, 		"thelikestream.com"
+set :port, 		2298
+set :repository,  	"ssh://#{domain}:#{port}/home/git/likestream.git"
+set :use_sudo,		false
+set :deploy_to		"/users/emcmanus/rails_apps/#{application}"
+set :scm, 		:git
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-set :deploy_to, "/home/git/#{application}"
-
-set :location, "thelikestream.com"
-
-role :web, location
-role :app, location
-role :db,  location, :primary => true
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+after "deploy", "deploy:cleanup"
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :start, :roles => :app do
+	run "touch #{current_release}/tmp/restart.txt
+  end
+
+  task :stop, :roles => :app do ; end
+  
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
