ade8c58d18e7af55f0f2e29d93801a8caf34adb1
Ed McManus <edmcmanus@champ.local>
2010-09-10 05:41:44 -0700

    More open graph tag fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def host_with_port
+    value = "#{request.host}"
+    value += ":#{request.port}" if request.port != 80
+    value
+  end
+  
+  def image_url(source) 
+     abs_path = image_path(source) 
+     unless abs_path =~ /^http/ 
+       abs_path = "http://#{host_with_port}#{abs_path}" 
+     end 
+     abs_path 
+  end
 end
