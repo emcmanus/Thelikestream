e26af95d90a16c97d4239df7d34402013057fb77
Ed McManus <edmcmanus@champ.local>
2010-09-26 09:57:23 -0700

    Missing files

---
+++ db/migrate/20100926162040_add_quality_flags_to_page.rb
@@ -0,0 +1,14 @@
+class AddQualityFlagsToPage < ActiveRecord::Migration
+  def self.up
+    # Show in favorites
+    add_column :pages, :show_in_favorites, :boolean, :default => false
+    # Display in Popular
+    add_column :pages, :show_in_popular, :boolean, :default => false
+    Pages.update_all('show_in_popular=false, show_in_favorites=false')
+  end
+
+  def self.down
+    remove_column :pages, :show_in_favorites
+    remove_column :pages, :show_in_popular
+  end
+end
